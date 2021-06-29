5ceffc42bd3f28182a40b6acfb12369a424af066
realone <emmamowarin3@gmail.com>
2021-06-29 21:06:23 +0100

    first commit

---
+++ customer_info/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from customer_info import views
+from django.contrib.auth.models import User
+
+app_name = 'customer_info'
+
+
+urlpatterns = [
+    path('',views.IndexView.as_view(),name='index'),
+    path('user_login', views.user_login,name='user_login'),
+    path('payment',views.PaymentListView.as_view(),name='payment'),
+    path('detail/<int:pk>/',views.CustomerDetailView.as_view(),name='detail'),
+    path('register', views.register,name='register')
+]
